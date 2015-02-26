4ebda7a3300290dc2cb4b4ae251f039270ce170a
aeldred <aeldred@gmail.com>
2015-02-25 21:32:22 -0600

    Andrew - update for ContactManager singleton

--- app/src/main/java/edu/umn/contactview/ContactManager.java
+++ app/src/main/java/edu/umn/contactview/ContactManager.java
@@ -1,17 +1,98 @@
 package edu.umn.contactview;
 
+import android.content.Context;
+import android.net.http.AndroidHttpClient;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.google.gson.Gson;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+
+import java.io.InputStreamReader;
+import java.util.List;
+
 /**
  * Created by ryan on 2/21/15.
  *
  * This where we will interface with the database
  */
 public class ContactManager {
-    private static ContactManager ourInstance = new ContactManager();
+    private static ContactManager ourInstance;
+    private static MainActivity activity;
+    private static Boolean isPopulated = Boolean.FALSE;
 
-    public static ContactManager getInstance() {
+    private static List<Contact> contacts;
+
+    public static ContactManager getInstance(Context aContext) {
+        if (aContext instanceof MainActivity) {
+            activity = (MainActivity)aContext;
+        }
+        if (!isPopulated) {
+            ourInstance = new ContactManager();
+            isPopulated = Boolean.TRUE;
+        } else {
+            activity.updateAdapter(contacts);
+        }
+
         return ourInstance;
     }
 
     private ContactManager() {
+        new GetContactTask().execute();
+    }
+
+    public static void clearContactManager() {
+        isPopulated = Boolean.FALSE;
+    }
+    public static List<Contact> getContacts() {
+        return contacts;
+    }
+
+    //This provides the basic functionality for retrieving all current contacts
+    //from web service.
+    private class GetContactTask extends AsyncTask<Void, Void, ServiceResult> {
+        private String URL_BASE = activity.getString(R.string.URL_BASE);
+        private String API_KEY = activity.getString(R.string.API_KEY);
+        private ServiceResult result;
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected ServiceResult doInBackground(Void... params) {
+            try {
+                AndroidHttpClient client = AndroidHttpClient.newInstance("Android", null);
+                HttpUriRequest request = new HttpGet(URL_BASE + "contacts" +
+                        "?key=" + API_KEY);
+                HttpResponse response = client.execute(request);
+                Gson gson = new Gson();
+                result = gson.fromJson(
+                        new InputStreamReader(response.getEntity().getContent()),
+                        ServiceResult.class);
+
+                client.close();
+                return result;
+            }
+            catch (Exception ex) {
+                Log.w("GetContactTask", "Error getting contact", ex);
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(ServiceResult result) {
+            super.onPostExecute(result);
+
+            //String test = result.toString();
+            //Log.w("onPostExecute","GSON Result: " + test);
+
+            contacts = result.getContacts();
+            activity.updateAdapter(contacts);
+
+        }
     }
 }

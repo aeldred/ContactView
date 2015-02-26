a66f0c7bac437cd4ed7f198912976dff1c72833c
trozymotto <ryan.trosvig@gmail.com>
2015-02-25 21:25:36 -0600

    ryan's stuff

--- app/src/main/java/edu/umn/contactview/ContactManager.java
+++ app/src/main/java/edu/umn/contactview/ContactManager.java
@@ -14,4 +14,22 @@ public class ContactManager {
 
     private ContactManager() {
     }
+
+    public Contact GetContact(String mId)
+    {
+        Contact mContact = new Contact();
+        mContact.setName("Yo mamma!");
+        mContact.setPhone("555-555-5555");
+        mContact.setEmail("yomamma@g.com");
+        mContact.setTitle("the boss");
+        mContact.setTwitterId("@yomamma");
+        mContact.set_id(mId);
+
+        return mContact;
+    }
+
+    public void UpdateContact(String mId, Contact mContact)
+    {
+        // Update the JSON data
+    }
 }
